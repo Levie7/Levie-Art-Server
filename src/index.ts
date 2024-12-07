b3f5dee5554a42ebf81c0fa78d2555603037f459
Amelia Maya Christina <46135065+Levie7@users.noreply.github.com>
2024-12-07 13:01:28 +1100

    setup router

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { NowRequest, NowResponse } from '@vercel/node';
 import express from "express";
 import mongoose from "mongoose";
 import cloudinary from "./config/cloudinary";
@@ -10,7 +11,13 @@ dotenv.config();
 
 const app = express();
 const upload = multer({ dest: "uploads/" });
-
+const router = express.Router();
+router.get("/", (req, res) => {
+    res.send("Levie Art");
+});
+  
+  
+app.use(router);
 app.use(cors());
 app.use(express.json());
 
@@ -50,3 +57,6 @@ app.post("/upload", upload.single("file"), async (req, res) => {
 });
 
 app.listen(5001, () => console.log("Server running on http://localhost:5001"));
+export default (req: NowRequest, res: NowResponse) => {
+    app(req, res);
+};
